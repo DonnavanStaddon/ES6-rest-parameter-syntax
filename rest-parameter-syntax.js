4431770188a7fc58d29d5b746139168daec0e1d8
Donnavan Stadd <donnystaddon@gmail.com>
2023-06-19 15:53:19 +0000

    Rest Parameter Syntax. ES6 Learning

--- rest-parameter-syntax.js
+++ rest-parameter-syntax.js
@@ -3,8 +3,28 @@
  * command node rest-parameter-syntax.js in the terminal
  */
 
-// Regular function call 
+// Regular function call
+const sumAll = (a, b, c) => a + b + c;
+let sum = sumAll(1, 2, 3);
+console.log("Sum:", sum);
 
-// Extra arguments are ignored
+// Extra arguments are ignored-"NOT What we want"
+let sum2 = sumAll(1, 2, 3, 4, 5, 6);
+console.log("Sum2:", sum2);
 
-// Function using ...rest
+//////////////////////////////////////////////////////
+
+// Function using ...rest. creating sumRest
+const sumRest = (a, b, c, ...rest) => {
+    let sum = a + b + c;
+    for (let i of rest) {
+        sum += i;
+    }
+
+    return sum;
+}
+//Adding to sumRest
+let sum3 = sumRest(1, 2, 3, 4, 5, 6);
+console.log("Sum3:", sum3);
+
+/////////////////////////////////////////////////////
